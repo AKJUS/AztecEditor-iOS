bd3eaccc72c24841fff4457ca431e46e451cba69
Gio Lodi <gio@mokacoding.com>
2025-06-05 13:57:06 +1000

    Bump minimum supported version to iOS 13
    After all, all clients of this app already support above iOS 13.
    
    Hopefully this will address the 12 vs 13 issue we're seeing in CI.
    
    https://buildkite.com/automattic/aztec-editor-ios/builds/211/steps/canvas?jid=019738a1-cb58-4c2d-84de-d8aa8f76a57b

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WordPress-AztecEditor-iOS",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "Aztec", targets: ["Aztec"]),
         .library(name: "HTMLParser", targets: ["HTMLParser"]),
